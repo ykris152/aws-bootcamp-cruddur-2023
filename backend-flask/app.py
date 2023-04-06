4c36a238540724fce5d9eb6f0a8367f4c6147116
ykris152 <ykris152@gmail.com>
2023-04-06 06:36:10 +0000

    3854

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -124,12 +124,28 @@ def rollbar_test():
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
-  user_handle  = request.json["user_handle"]
-  model = MessageGroups.run(user_handle=user_handle)
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
+
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    cognito_user_id = claims['sub']
+    # app.logger.debug(claims['username'])
+    data = MessageGroups.run(cognito_user_id=cognito_user_id)
+
+    if model['errors'] is not None:
+      return model['errors'], 422
+    else:
+      return model['data'], 200
+
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = MessageGroups.run()
+    return {}, 401
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
