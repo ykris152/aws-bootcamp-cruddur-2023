cc6803018b5a248a107456d4a555bb0bd8e0b2e6
ykris152 <ykris152@gmail.com>
2023-04-07 11:39:51 +0000

    fixed the seed data with own data

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.com','andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.com','bayko' ,'MOCK');
+  ('ykris152', 'ykris152@gmail.com','ykris152' ,'MOCK');
+  -- ,
+  -- ('Andrew Bayko', 'bayko@exampro.com','bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'ykris152' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
