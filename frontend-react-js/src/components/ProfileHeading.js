5393af5142842515e824335ac617b6be27925a62
ykris152 <ykris152@gmail.com>
2023-07-26 08:10:21 +0000

    one title style-change

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -2,25 +2,29 @@ import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
 export default function ProfileHeading(props) {
-    const backgroundImage = 'url("/")';
-    const styles = {
-        backgroundImage: backgroundImage,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    };
-
+  const backgroundImage = 'url("https://assets.thebanana.click/banners/360_F_495286577_rpsT2Shmr6g81hOhGXALhxWOfx1vOQBa.jpg")';
+  const styles = {
+    backgroundImage: backgroundImage,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
   return (
-    <div className='profile_heading'>
-        <div className='title'>{props.profile.display_name}</div>
-        <div className="cruds_count">{props.profile.cruds_count}Cruds</div>
-        <div className="banner" styles={styles}>
-            <div className="avatar">
-                <img src="https://assets.thebanana.click/avatars/20230121_190025.jpg"></img>
-            </div>
-        </div>
-        <div className="display_name">{props.display_name}</div>
-        <div className="handle">@{props.handle}</div>
-        <EditProfileButton setPopped={props.setPopped}></EditProfileButton>
+  <div className='activity_feed_heading profile_heading'>
+    <div className='title'>{props.profile.display_name}</div>
+    <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
+    <div class="banner" style={styles} >
+      <div className="avatar">
+        <img src="https://assets.thebanana.click/avatars/20230121_190025.jpg"></img>
+      </div>
     </div>
+    <div class="info">
+      <div class='id'>
+        <div className="display_name">{props.profile.display_name}</div>
+        <div className="handle">@{props.profile.handle}</div>
+      </div>
+      <EditProfileButton setPopped={props.setPopped} />
+    </div>
+
+  </div>
   );
 }
